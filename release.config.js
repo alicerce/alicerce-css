a4b83f0f62aef6f45884264ea96edbed59012a0d
Luiz tanure <letanure@gmail.com>
2019-06-05 14:54:33 +0200

    fix: test gh pages sematinc release

--- release.config.js
+++ release.config.js
@@ -1,4 +1,10 @@
 module.exports ={
+  'verifyConditions': [
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git',
+    '@qiwi/semantic-release-gh-pages-plugin'
+  ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
@@ -14,4 +20,13 @@ module.exports ={
     '@semantic-release/npm',
     ['@semantic-release/git', {message: `chore(release): \${nextRelease.version} [skip ci]`}],
   ],
+  'publish': [
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    {
+      'path': '@qiwi/semantic-release-gh-pages-plugin',
+      'msg': 'docs: updated docs',
+      'branch': 'gh-pages'
+    }
+  ]
 }
